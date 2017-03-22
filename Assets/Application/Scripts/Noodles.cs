276822b5b5f84cb200ce06f2d68e1dd2d9f689c1
kazumalab <kazuma-arashi@hotmail.co.jp>
2017-03-22 14:39:55 +0900

    Add Scripts.

---
+++ Assets/Application/Scripts/Noodles.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Noodles : MonoBehaviour {
+
+	private Rigidbody[] noodles;
+	[SerializeField]private bool isGround = false;
+
+	// Use this for initialization
+	void Start () {
+		int count = transform.childCount;
+		noodles = new Rigidbody[count];
+
+		for (int i = 0; i < count; i++) {
+			noodles [i] = transform.GetChild (i).GetComponent<Rigidbody> ();
+		}
+	}
+
+	public void AddPower (Vector3 p)
+	{
+		for (int i = 0; i < transform.childCount; i++) {
+			noodles [i].AddForce (p);
+		}
+
+	}
+}
--- Assets/Application/Scripts/Test/AccelerationTest.cs
+++ Assets/Application/Scripts/Test/AccelerationTest.cs
@@ -4,21 +4,31 @@ using UnityEngine;
 
 public class AccelerationTest : MonoBehaviour {
 
-	private float Power = 10f;
-	private GameObject[] noodles;
+	[SerializeField]private float MAGUNITUDE = 2F;
+	private float Power = 50f;
+	private Noodles[] noodles;
 
 	private void Start () {
-		noodles = GameObject.FindGameObjectsWithTag ("Noodle");
+		GameObject[] objs = GameObject.FindGameObjectsWithTag ("Noodle");
+		noodles = new Noodles[objs.Length];
+		for (int i = 0; i < objs.Length; i++) {
+			noodles [i] = objs [i].GetComponent<Noodles> ();
+		}
 	}
 
 	// Update is called once per frame
 	void Update () {
-		Vector3 dy = Input.acceleration;
+		Vector3 acceleration = Input.acceleration;
+		float magnitude = acceleration.magnitude;
+		print (magnitude);
+		if (magnitude > MAGUNITUDE) {
+			AddPower (-acceleration);
+		}
 	}
 
-	void AddPower (float pow) {
+	void AddPower (Vector3 pow) {
 		for (int i = 0; i < noodles.Length; i++) {
-	
-		}	
+			noodles [i].AddPower (pow * Power);
+		}
 	}
 }
