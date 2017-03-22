dd84c627102d53c2713b6b33a0cf2feffc3b529c
kazumalab <kazuma-arashi@hotmail.co.jp>
2017-03-22 12:40:43 +0900

    Add Script and Noodle Scene.

---
+++ Assets/Application/Scripts/Test/AccelerationTest.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AccelerationTest : MonoBehaviour {
+
+	private float Power = 10f;
+	private GameObject[] noodles;
+
+	private void Start () {
+		noodles = GameObject.FindGameObjectsWithTag ("Noodle");
+	}
+
+	// Update is called once per frame
+	void Update () {
+		Vector3 dy = Input.acceleration;
+	}
+
+	void AddPower (float pow) {
+		for (int i = 0; i < noodles.Length; i++) {
+	
+		}	
+	}
+}
